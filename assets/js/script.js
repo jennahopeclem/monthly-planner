27c01393284dda92c09febbd9ddcfc7c9dfb6d08
azimezn <117550865+azimezn@users.noreply.github.com>
2022-12-07 11:06:38 -0500

    weather api is getting only national holidays

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ async function getiss() {
 getiss()
 
 function weatherData() {
-    var weatherAPI = 'https://calendarific.com/api/v2/holidays?&api_key=0f6f3c056e70ebca75cebdcc5cbbadf546e7c0c1&country=US&year=2022';
+    var weatherAPI = 'https://calendarific.com/api/v2/holidays?&api_key=0f6f3c056e70ebca75cebdcc5cbbadf546e7c0c1&country=US&year=2022&type=national';
 
     fetch(weatherAPI)
         .then(function (response) {
