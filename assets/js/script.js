1f8960236bafbd4a0f182f18df0b97a889cb1653
azimezn <117550865+azimezn@users.noreply.github.com>
2022-12-07 10:35:36 -0500

    working on the weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
 
 
-https://calendarific.com/api/v2/holidays?&api_key=0f6f3c056e70ebca75cebdcc5cbbadf546e7c0c1&country=US&year=2022
+//https://calendarific.com/api/v2/holidays?&api_key=0f6f3c056e70ebca75cebdcc5cbbadf546e7c0c1&country=US&year=2022
+
+function weatherData () {
+    var weatherAPI = 'https://api.github.com/orgs/nodejs/repos';
+
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data)
+   } )
+
