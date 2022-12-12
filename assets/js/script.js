d45d103b6c3ac0f165454bf5adcc4699d361937c
jennahopeclem <jennahopeclem@gmail.com>
2022-12-12 13:25:15 -0500

    events show on sidebar list now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,3 @@
-
-//team 1 Atlanta Falcons
-//team 2 Buffalo Bills
-//team 3 Chicago Bears
-//team 4 Cincinnati Bengals
-//team 5 Cleveland Browns
-//team 6 Dallas Cowboys
-//team 7 Denver Broncos
-//team 8 Detroit Lions
-//team 9 Green Bay Packers
-//team 10 Tennessee Titans
-//team 11 Indianapolis Colts
-//team 12 Kansas City Chiefs
-//team 13 Las Vegas Raiders
-//team 14 Los Angeles Rams
-//team 15 Miami Dolphins
-//team 16 Minnesota Vikings
-//team 17 New England Patriots
-//team 18 New Orleans Saints
-//team 19 New York Giants
-//team 20 New York Jets
-//team 21 Philadelphia Eagles
-//team 22 Arizona Cardinals
-//team 23 Pittsburgh Steelers
-//team 24  Los Angeles Charger
-//team 25 San Francisco 49ers
-//team 26 Seattle Seahawks
-//team 27 Tampa Bay Buccaneers
-//team 28 Washington Commanders
-//team 29 Carolina Panthers
-//team 30 Jacksonville Jaguars
 var c = dayjs().format("YYYY-MM-DD")
 var todayDate = dayjs().format("MMMM DD, YYYY");
 //for comparing and displaying
@@ -37,7 +6,7 @@ var curMonthNumber = dayjs().format('MM')
 var curDay = dayjs().format('D');
 var curYear = dayjs().format('YYYY');
 var curWeekDay = dayjs().format('dddd');
-//vars we may need when formating the month dates
+//vars we need when formating the month dates
 var weekdays = [0, 1, 2, 3, 4, 5, 6];
 var numOfDays = dayjs().daysInMonth(); // number of days in the month
 var firstDayOfMonth = dayjs().startOf('month').$d;
@@ -45,13 +14,13 @@ var dateString = firstDayOfMonth.toLocaleDateString();  //how to get the weekday
 var firstWDofMonth = dayjs().startOf('month').day();
 var paddingDays = weekdays[firstWDofMonth]; //how many black days we are going to have in beg of month
 var calDate;
+var dayBlock;
 var dayNumber;
-var calDate;
-var dataDouble;
-var dataSingle;
-
+var holidayList = [];
 var calendar = document.querySelector('#calendar');
-
+var sidebarList = document.querySelector('.sidebar-list');
+var calEvent;
+var plusSign = document.querySelector(".plus-sign");
 
 function displayDates() {
     //displays month and year at top of calendar
@@ -63,117 +32,118 @@ function displayDates() {
     //displays todays date and month into box on side of calendar
     var curMonthDay = document.querySelector('#curMonthDay');
     curMonthDay.textContent = curMonth + ' ' + curDay;
-
     //function to compare the date number on the calendar to the current date and add class '.today'
-
-    //how are we going to update the calendar dates???? and making sure it starts on correct weekday???? 
     for (let i = 1; i <= paddingDays + numOfDays; i++) {
-        var dayBlock = document.createElement('div');
+        dayBlock = document.createElement('div');
         dayBlock.classList.add('calendar-day');
         calDate = document.createElement('div');
         calDate.classList.add('calendar-date');
-    
-
         if (i > paddingDays) {
-            calDate.textContent = i - paddingDays; //supposed to add the date numbers... but is not 
+            calDate.textContent = i - paddingDays;
             calDate.setAttribute('id', curYear + "-" + curMonthNumber + "-" + calDate.textContent);
-
             //add eventlistener to change the aside box view?
         } else {
             dayBlock.classList.add('inactive');
+            calDate.setAttribute('id', 'n/a')
         }
         calendar.appendChild(dayBlock);
         dayBlock.appendChild(calDate);
-
     }
-
-    // dayNumber = calendar.dayBlock.calDate.value
-    // console.log(dayNumber)
-
+    //applies different color to today's date
+    $('.calendar-date').map(function () {
+        if (dayjs().format('YYYY-MM-D') == $(this).attr('id')) {
+            //console.log(dayjs().format('YYYY-MM-D'), $(this).attr('id'));
+            $(this).addClass('today');
+        }
+    })
 }
 
 displayDates();
 
-function run(t) {
 
-    var nflApi = `http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/2022/teams/${t}/events?lang=en&region=us`
-    async function getiss() {
-        const response = await fetch(nflApi);
-        const data = await response.json();
-        const { items } = data;
+async function run(t) {
+    var nflApi = `https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/2022/teams/${t}/events?lang=en&region=us`
 
-
-        for (var i = 0; i < items.length; i++) {
-            var team = items[i];
-
+    const response = await fetch(nflApi);
+    const data = await response.json();
+    const { items } = data;
+    for (var i = 0; i < items.length; i++) {
+        var team = items[i];
         fetch(team.$ref)
             .then(function (response) {
                 // console.log(team.$ref); //console logs the team links
                 // console.log(response);
                 return response.json();
             }).then(function (data) {
-                var a = $("<p1>")
-                var dates = data.date.split("T")[0]
-                a.textContent=dates
-                // console.log(dates)
-                
-                console.log(a)
-if (a.textContent == c) {
-        console.log("mathch")
-        
+                //console.log(data);
+                var a = $("<p1>");
+                var dates = data.date.split("T")[0];
+                var times = data.date.split("T")[1].split("Z")[0];
+                a.textContent = dates;
+                // whats this?
+                $('.calendar-date').map(function () {
+                    if (a.textContent == $(this).attr('id')) {
+                        console.log(a.textContent, $(this).attr('id'));
+                        calEvent = document.createElement("p");
+                        document.getElementById($(this).attr('id')).appendChild(calEvent);
+                        calEvent.textContent = data.name + " " + times;
+                        console.log(calEvent)
+                    } else {
+                    };
+                })
+            }
+            )
+
     }
-    else{
-        console.log("boooo")
-        console.log(a.textContent)
-        console.log(c)
-    }
-                // $('.calendar-date').append(a.textContent)
-                // console.log(calendar)
-
-                // console.log(data); //returns the data from the team links
-            })
-
-        }
-
-    
-
-    } getiss()
 }
-//getiss()
-
 
 function holidayData() {
-
-    var holidayAPI = 'https://calendarific.com/api/v2/holidays?&api_key=0f6f3c056e70ebca75cebdcc5cbbadf546e7c0c1&country=US&year=2022&type=national';
-
+    var holidayAPI = 'https://calendarific.com/api/v2/holidays?&api_key=a650bf2c0b0cdf1c919ef9a9cef5fba2a18c8ef1&country=US&year=2022&type=national';
     fetch(holidayAPI)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data)
-
+            //console.log(data)
             for (var i = 0; i < data.response.holidays.length; i++) {
-
-                console.log($(this).attr('id'))
+                let holidayInfo = {
+                    date: dayjs(data.response.holidays[i].date.iso).format("YYYY-MM-D"),
+                    name: data.response.holidays[i].name
+                }
 
                 //if holiday date is same as calendar date, display holiday
                 $('.calendar-date').map(function () {
-                    if (dayjs(data.response.holidays[i].date.iso).format("YYYY-MM-D") == $(this).attr('id')) {
-                        console.log("same date")
+                    if (holidayInfo.date == $(this).attr('id')) {
+                        // console.log(holidayInfo.date, $(this).attr('id'));
+                        var calEvent = document.createElement("p");
+                        //calDate.appendChild(calEvent) was calling the final most calDate. This looks up the specific calDate by id rather than call the last made.
+                        document.getElementById($(this).attr('id')).appendChild(calEvent);
+                        calEvent.textContent = holidayInfo.name;
+                    }
+                })
+            }
+
+            for (var i = 0; i < holidayList.length; i++) {
+
+                $('.calendar-date').map(function () {
+
+                    if (holidayList[i] == $(this).attr('id')) {
+
                         var calEvent = document.createElement("p");
                         calDate.appendChild(calEvent);
                         calEvent.textContent = data.response.holidays[i].name
-                    } else {
-                    };
+
+                    }
                 })
 
+
             }
+
+
+
         })
 }
-
-holidayData();
-
+// holidayData();
 
 var Dd = document.querySelector("#Sd");
 var test = document.querySelector(".Sportsdropdown")
@@ -181,9 +151,40 @@ var test = document.querySelector(".Sportsdropdown")
 Dd.addEventListener("click", function (event) {
     var element = event.target;
     console.log(element)
-
     var t = element.getAttribute("id");
-
     run(t)
     console.log(t)
-});
+});  // drop downs arent on clicks they are on changes.
+
+plusSign.addEventListener('click', addTodo);
+function addTodo() {
+
+}
+
+
+
+
+$(document).on('click', '.calendar-day', displayDay)
+function displayDay(event) {
+    // event.preventDefault();
+    $('.sidebar-list').empty();
+    // sidebarList.innerHTML = "";
+    var whatsTheDay = $(this);
+    console.log(whatsTheDay);
+
+    curWeekday.innerHTML = dayjs($(this).children().attr("id")).format("dddd");
+    curMonthDay.innerHTML = dayjs($(this).children().attr("id")).format("MMMM D");
+
+
+    var dailyList = document.createElement("li");
+    dailyList.classList.add("sidebar-list-item");
+    sidebarList.appendChild(dailyList);
+    dailyList.textContent = $(this)[0].children[0].children[0].textContent || "";
+    // console.log($(this).children().children().innerHTML);
+    // console.log(calEvent);
+
+}
+
+
+
+
